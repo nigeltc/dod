e01b27fa4f0cd4cb1a6b5101a035edd181e0f162
nigeltc <nigeltc@gmail.com>
2021-08-22 11:52:56 -0400

    Add tiles

--- game.js
+++ game.js
@@ -1,25 +1,47 @@
 const scene = {
     preload: function() {
-	this.load.bitmapFont("arcade", "font/arcade.png", "font/arcade.xml");
+	this.load.spritesheet(
+	    "tiles",
+	    "assets/colored.png",
+	    {
+		frameWidth: 16,
+		framHeight: 16,
+		spacing: 1
+	    });
     },
     create: function() {
-	this.player = this.add.bitmapText(400, 300, "arcade", "X")
-	    .setOrigin(0.5);
-	this.cursors = this.input.keyboard.createCursorKeys();
+	let level = [
+	    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+	    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+	    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+	    [1, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+	    [1, 0, 0, 0, 2, 0, 0, 0, 0, 1],
+	    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+	    [1, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+	    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+	    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+	    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]];
+	const wall = 554;
+	const skeleton = 480;
+	const floor = 0;
+	level = level.map(r => r.map(t => t == 1 ? wall : t == 2 ? skeleton : floor));
+
+	const tileSize = 16;
+	const config = {
+	    data: level,
+	    tileWidth: tileSize,
+	    tileHeight: tileSize
+	};
+	const map = this.make.tilemap(config);
+	const tileset = map.addTilesetImage(
+	    "tiles",
+	    "tiles",
+	    tileSize,
+	    tileSize,
+	    0, 1);
+	const ground = map.createStaticLayer(0, tileset, 0, 0);
     },
     update: function() {
-	if (this.cursors.left.isDown) {
-	    this.player.x -= 10;
-	}
-	if (this.cursors.right.isDown) {
-	    this.player.x += 10;
-	}
-	if (this.cursors.up.isDown) {
-	    this.player.y -= 10;
-	}
-	if (this.cursors.down.isDown) {
-	    this.player.y += 10;
-	}
     }
 };
 
